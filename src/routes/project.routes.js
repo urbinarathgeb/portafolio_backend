6f0c55797bd3e75f2389e90e1cd0c3298cc60d62
Javier Urbina Rathgeb <urbinarathgeb@gmail.com>
2026-06-06 19:04:04 -0400

    feat: crear rutas de projects con validación

---
+++ src/routes/project.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as projectController from '../controllers/project.controller.js';
+import { validate } from '../middlewares/validate.middleware.js';
+import { createProjectSchema, updateProjectSchema, idParamSchema } from '../validations/project.validation.js';
+
+const router = Router();
+
+router.get('/projects', projectController.list);
+router.get('/projects/:id', validate(idParamSchema, 'params'), projectController.detail);
+router.post('/projects', validate(createProjectSchema), projectController.create);
+router.put('/projects/:id', validate(idParamSchema, 'params'), validate(updateProjectSchema), projectController.update);
+router.delete('/projects/:id', validate(idParamSchema, 'params'), projectController.remove);
+
+export default router;
